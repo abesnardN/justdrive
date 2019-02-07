65dd5cd7bd86af1f114f327ecd12f5fd9e54c4d9
thiennot <ophelie.thiennot@etu.univ-orleans.fr>
2019-02-07 14:22:47 +0100

    react sous apache

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
 
-class App extends Component {
+// Obtain the root
+    const rootElement = document.getElementById('root')
+// Create a ES6 class component
+    class ShoppingList extends React.Component {
+// Use the render function to return JSX component
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
@@ -22,7 +26,32 @@ class App extends Component {
         </header>
       </div>
     );
-  }
+}
 }
 
-export default App;
+// Create a function to wrap up your component
+function App(){
+  return(
+    <div className="App">
+      <header className="App-header">
+        <img className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
+}
+// Use the ReactDOM.render to show your component on the browser
+    ReactDOM.render(
+      <App />,
+      rootElement
+    )
