e98543b8c5cbbcab8d36ac47a07a462495f55487
thiennot <ophelie.thiennot@etu.univ-orleans.fr>
2019-04-05 16:49:15 +0200

    erreur sur la page de connexion

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,6 +14,6 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-            return $this->redirectToRoute('connexion_index');
+            return $this->redirectToRoute('connexion');
     }
 }
