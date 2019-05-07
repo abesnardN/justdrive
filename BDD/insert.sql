c00c44a19461482f0dddb6a6544fa705c871663c
Thiennot <ophelie.thiennot@hotmail.fr>
2019-05-07 16:35:10 +0200

    maj données insertion

--- BDD/insert.sql
+++ BDD/insert.sql
@@ -1,7 +1,7 @@
 insert into etatvehicule value(1,'propre');
   insert into etatvehicule value(2,'sale');
     insert into vehicule(idVehicule, modele, kilometrage, marque, nbPlace,immatriculation,datePremiereCirculation,fkEtat)values('1','megane',100000, 'Renaut',5,'DD 153 JJ',curdate(),1)
-
+INSERT INTO pays values('FR','France');
 /*Adresse*/
 INSERT INTO adresse(idAdresse,numRue,nomRue,ville,codePostal,fkPays,latitude,longitude)
   VALUES('1','1','rue de l eglise','Nantes','44000','FR','1.15','4878')
@@ -14,3 +14,7 @@ INSERT INTO adresse(idAdresse,numRue,nomRue,ville,codePostal,fkPays,latitude,lon
 
 
 INSERT INTO user(mail, password, dateInscription) VALUES ('mail@mail.fr','aaa',CURDATE())
+
+
+INSERT INTO trajet(fkUser, adresseDepart, addresseArrive, fkVehicule, dateDepart, dateArrive, kilometrage, commentaireConducteur, commentaireInterne, pointRDVDepart, pointRDVArrive, fkEtat)
+VALUES (1,1,4,1,CURDATE(),CURDATE(),'10000','il me faut une camionnette pour le materiel','Ok',null,null,1);
